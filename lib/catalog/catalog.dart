ab1b803ff644b449319bd093647ea94198f350dc
smit <monparasmit1@gmail.com>
2022-09-08 18:39:18 +0530

    make the catalog in the app to the list view

---
+++ lib/catalog/catalog.dart
@@ -0,0 +1,8 @@
+class Catalog {
+  int id;
+  String title;
+  String subTitle;
+  String salary;
+
+  Catalog({required this.id, required this.title, required this.subTitle,required this.salary});
+}
--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:ms_creation/catalog/catalog.dart';
 
 class HomePage extends StatelessWidget {
-  String name;
-  HomePage({super.key,required this.name});
+  final String name;
+  const HomePage({super.key, required this.name});
 
   @override
   Widget build(BuildContext context) {
+    List catalogItem = makeCatalog();
+
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text("Home"),
-      ),
-      body: Center(
-        child: Text("My Name Is $name"),
-      ),
-    );
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text("Home"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 8.0, left: 16.0, right: 16.0),
+          child: ListView.builder(
+            itemBuilder: ((context, index) {
+              return makeCardCatalog(catalogItem[index]);
+            }),
+            itemCount: catalogItem.length,
+          ),
+        ));
   }
+}
+
+Widget makeCardCatalog(Catalog catalog) {
+  return Card(
+    margin: const EdgeInsets.symmetric(vertical: 4.0),
+    elevation: 3,
+    child: ListTile(
+      contentPadding:
+          const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+      leading: Image.asset("assets/images/working.png"),
+      title: Text(catalog.title),
+      subtitle: Text(catalog.subTitle),
+      trailing: Text("\$${catalog.salary}"),
+    ),
+  );
+}
+
+List makeCatalog() {
+  List list = List.generate(
+      20,
+      (index) => Catalog(
+          id: 1,
+          title: "Working Employee",
+          subTitle: "Working Smit",
+          salary: "1000"));
+  return list;
 }
